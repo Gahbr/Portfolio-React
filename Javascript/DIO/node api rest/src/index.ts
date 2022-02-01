ca56567c87b4b590b26437f6544daeab5596b651
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-02-01 17:08:23 -0300

    automatizando o servidor para identificar alteracoes no codigo

--- Javascript/DIO/node api rest/src/index.ts
+++ Javascript/DIO/node api rest/src/index.ts
@@ -2,10 +2,10 @@ import express, {Request,Response,NextFunction} from 'express'; //gerenciador de
 
 const app = express();
 app.get('/status', (req: Request, res: Response, next:NextFunction)=> {
-    res.status(200).send({foo:'bar'})
+    res.status(200).send({foo:'parakeet keetson'})
 })
 
 app.listen (3000, ()=>{
-    console.log("Aplicacao executando na porta 3000.");
+    console.log("Aplicação executando na porta 3000.");
     
 })
