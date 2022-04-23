31060c4a56a852592fbcb822d1e2345910ff8f23
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-04-23 16:05:01 -0300

    Updates

---
+++ precache-manifest.afe9acd52319693142b61c9a3fa982db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ccee2ce4fbb1059011628e5eaa03d6",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "e52613222cdf8f77e407",
+    "url": "/Portfolio-React/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d4c71dd9a9dde2fc0775",
+    "url": "/Portfolio-React/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "e52613222cdf8f77e407",
+    "url": "/Portfolio-React/static/js/2.33a622d2.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio-React/static/js/2.33a622d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c71dd9a9dde2fc0775",
+    "url": "/Portfolio-React/static/js/main.187eea69.chunk.js"
+  },
+  {
+    "revision": "96997b7f1d85551e46bb",
+    "url": "/Portfolio-React/static/js/runtime-main.b1d1f1aa.js"
+  }
+]);
