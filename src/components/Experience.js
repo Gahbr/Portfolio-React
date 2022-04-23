2811153399095ab71afa99fe8e6f9b02dab74756
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-04-23 14:56:51 -0300

    finishing the portuguese version

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -37,7 +37,7 @@ class Experience extends Component {
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
+            icon={<i className="fab fa-html5 experience-icon"></i>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
